221856fa5787f4c7a6aa2772270e8192bb64307f
Werrit <binphuc7@gmail.com>
2026-04-16 14:51:56 +0700

    add js

---
+++ quiz_data.js
@@ -0,0 +1,23 @@
+const QUIZ_DATA = [
+  {
+    "id": "1",
+    "module": "Module 01: Hiểu biết về CNTT cơ bản",
+    "question": "Phần cứng máy tính là gì?",
+    "option_a": "Các bộ phận cụ thể của máy tính về mặt vật lý như màn hình, chuột, bàn phím,...",
+    "option_b": "Các chương trình phần mềm được cài đặt trên máy tính",
+    "option_c": "Mạng lưới các máy tính được kết nối với nhau",
+    "option_d": "Hệ điều hành của máy tính",
+    "correct": "A"
+  },
+  {
+    "id": "2",
+    "module": "Module 01: Hiểu biết về CNTT cơ bản",
+    "question": "Lựa chọn nào cho thấy máy tính xách tay có lợi thế hơn so với máy tính để bàn?",
+    "option_a": "Máy tính xách tay có thể mang đi bất kì nơi đâu",
+    "option_b": "Máy tính xách tay có hiệu suất cao hơn",
+    "option_c": "Máy tính xách tay có giá rẻ hơn",
+    "option_d": "Máy tính xách tay dễ nâng cấp hơn",
+    "correct": "A"
+  },
+  // ... rest of your questions
+];
